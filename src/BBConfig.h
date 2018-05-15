89c58809a0f2cfba824f1664e4b5aa339d7306ab
Manny Garcia <supermanny@me.com>
2018-05-14 20:35:04 -0700

    Added S2S configurations.

--- src/BBConfig.h
+++ src/BBConfig.h
@@ -1,15 +1,5 @@
 #ifndef BB8_CONFIG_H_
 #define BB8_CONFIG_H_
-
-// get data types
-#include <Arduino.h>
-
-/**
-* SoundFX configuration
-*/
-#define SFX_RST 37 // PIN
-#define SFX_SERIAL &Serial3 // Serial Port
-
 /**
 * Dome position and spin settings
 */
@@ -31,6 +21,10 @@
 #define MAIN_DRIVE_FWD 13
 #define MAIN_DRIVE_REV 12
 #define S2S_LEAN_POT A0
+#define S2S_LEAN_OFFSET -38 // Add to equal 512
+#define S2S_ENABLE_DRV 33 // enable pin for S2S
+#define S2S_LEFT_DRV 6 //FWD pin on motor controller
+#define S2S_RIGHT_DRV 7 //REV pin on motor controller
 
 /**
 * Sound FX
@@ -41,5 +35,15 @@
 #define SFX_BAUD_RATE 9600
 #define SFX_ACT_PIN 35
 #define SFX_FADE_PIN A2
+
+/**
+* DEBUGGING FLAGS
+* These exist in the headers of the project and are repeated here for usability.
+**/
+//#define DEBUG_CONTROL
+#define DEBUG_DRIVE_MOVEMENT
+//#define DEBUG_DOME_MOVEMENT
+//#define DEBUG_SFX
+//#define DEBUG_IMU
 
 #endif //BB8_CONFIG_H_
