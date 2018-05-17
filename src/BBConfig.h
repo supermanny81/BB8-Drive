15bd2e92ce83fe5b055fae8bc593f8c0df888751
Manny Garcia <supermanny@me.com>
2018-05-16 21:19:01 -0700

    Added a macro to load custom config files.  No longer need to change source code that is under revision control.

--- src/BBConfig.h
+++ src/BBConfig.h
@@ -1,6 +1,19 @@
 #ifndef BB8_CONFIG_H_
 #define BB8_CONFIG_H_
 /**
+*  DO NOT OVERRIDE VALUES IN THIS FILE!!!
+*
+*  This is a default reference for all of the possible configurations you can
+*  change in your BB unit.  This file will change over time and it's defaults
+*  will likely overwrite changes you have made.
+*
+*  To change any config items create a file called MyConfig.h in the same
+*  folder as your .ino file.  Options copied there will override those defined
+*  here and importantly will not overriden the next time you update the code.
+*/
+
+
+/**
 * Dome position and spin settings
 */
 #define DOME_RIGHT_SERVO_PIN 5
@@ -21,8 +34,8 @@
 #define MAIN_DRIVE_FWD 13
 #define MAIN_DRIVE_REV 12
 #define S2S_LEAN_POT A0
-#define S2S_LEAN_MIN 380 // Add to equal Max POT reading leaning to the right
-#define S2S_LEAN_MAX 600 // Max POT reading leaning to the right
+#define S2S_LEAN_MIN 360 // Add to equal Max POT reading leaning to the right
+#define S2S_LEAN_MAX 580 // Max POT reading leaning to the right
 #define S2S_ENABLE_DRV 33 // enable pin for S2S
 #define S2S_LEFT_DRV 6 //FWD pin on motor controller
 #define S2S_RIGHT_DRV 7 //REV pin on motor controller
@@ -45,9 +58,21 @@
 * These exist in the headers of the project and are repeated here for usability.
 **/
 //#define DEBUG_CONTROL
-#define DEBUG_DRIVE_MOVEMENT
+//#define DEBUG_DRIVE_MOVEMENT
 //#define DEBUG_DOME_MOVEMENT
 //#define DEBUG_SFX
 //#define DEBUG_IMU
+
+#if defined(__has_include)
+  #if __has_include("MyConfig.h")
+    #include "MyConfig.h"
+  #else
+    #warning "       DO NOT OVERRIDE VALUES IN THIS FILE"
+    #warning "--------------------------------------------------"
+    #warning "A custom configuration header does not exist."
+    #warning "Create a file called MyConfig.h and ovverride the "
+    #warning "directives (config items) listed here."
+  #endif
+#endif
 
 #endif //BB8_CONFIG_H_
